5692c47fdc0baaa42481bfa208322d6954e5b58d
Sergio De Luca <sdlbsso@gmail.com>
2022-03-16 21:53:48 -0300

    barra de menú, inicio del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DominicodeComponent } from './pages/dominicode/dominicode.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'dominicode', component: DominicodeComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'store';
+
+
+  getName(): void {
+    console.log("Hola  fff");
+  }
+
 }
